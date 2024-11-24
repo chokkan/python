03319387445460480a8480d5903f7ce9d4128a0e
Naoaki Okazaki <okazaki@c.titech.ac.jp>
2024-11-24 14:10:55 +0900

    Updated for the year 2024.

--- tools/update-license.py
+++ tools/update-license.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     for src in glob.glob('*.ipynb'):
         with open(src) as fi:
             content = fi.read()
-        content = content.replace('Copyright 2020-2021', 'Copyright 2020-2022')
-        content = content.replace('"copyrightYear": 2021,', '"copyrightYear": 2022,')
+        content = content.replace('Copyright 2020-2022', 'Copyright 2020-2024')
+        content = content.replace('"copyrightYear": 2022,', '"copyrightYear": 2024,')
         with open(src, 'w') as fo:
             fo.write(content)
